3acabff12a4463295f4925551fbe35865fb492f1
Magdalena Czajka <magdalena.czajka94@gmail.com>
2020-05-26 21:50:39 +0200

    add configuration for db using flask_migrations and sqlalchemy

--- idoctor/__init__.py
+++ idoctor/__init__.py
@@ -1,16 +1,26 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'dbs', 'idoctor.db')
 
 
 def create_app():
     idoctor = Flask(__name__)
+    idoctor.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
 
     from idoctor.views.main_views import main_bp
 
     idoctor.register_blueprint(main_bp)
 
+    db.init_app(idoctor)
+    migrate = Migrate(app=idoctor, db=db)
+
     return idoctor
 
 
 app = create_app()
-
-
