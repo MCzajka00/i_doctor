b1bb2a5c340d5b47050f40f10a346bbd4d63b907
Magdalena Czajka <magdalena.czajka94@gmail.com>
2020-05-26 21:54:09 +0200

    add configuration for flask_wtf

--- idoctor/__init__.py
+++ idoctor/__init__.py
@@ -12,6 +12,7 @@ db_path = os.path.join(basedir, 'dbs', 'idoctor.db')
 def create_app():
     idoctor = Flask(__name__)
     idoctor.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+    idoctor.config['SECRET_KEY'] = b'b\xad\x9e\xcc%""\xc2Yj(\x0e\xa5>\x16\x18'
 
     from idoctor.views.main_views import main_bp
 
